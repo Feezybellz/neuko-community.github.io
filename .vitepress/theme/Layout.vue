1381cc824eb9002a646cecf11c5fd1192ff19a2f
Jerry <jerry@avmillabs.com>
2025-12-08 21:10:34 -1000

    fix: restore homepage layout and content (index.md, Layout.vue, HomeBanner)

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -13,14 +13,8 @@ const { frontmatter } = useData()
   <Layout class="custom-layout">
     <template #home-hero-before>
       <div class="hero-spacer"></div>
-      <!-- <HomeBanner v-if="frontmatter.layout === 'home' && !frontmatter.hide_home_elements" /> -->
-
-      <!-- <HomeContent v-if="frontmatter.layout === 'home' && !frontmatter.hide_home_elements" /> -->
-
-
-
-
-
+      <HomeBanner v-if="frontmatter.layout === 'home' && !frontmatter.hide_home_elements" />
+      <HomeContent v-if="frontmatter.layout === 'home' && !frontmatter.hide_home_elements" />
     </template>
     <template #layout-bottom>
       <ScrollingBar v-if="frontmatter.layout === 'home' && !frontmatter.hide_scrolling_bar" />
